ffb1a293e7ff4aac2e00f44e1ae03f08c49de165
Veikko Karsikko <veikko88@gmail.com>
2015-04-29 14:49:00 +0300

    Update angular-no-captcha version

--- public/libs/angular-no-captcha/src/angular-no-captcha.js
+++ public/libs/angular-no-captcha/src/angular-no-captcha.js
@@ -1,91 +1,102 @@
 'use strict';
 
-angular.module('noCAPTCHA', [])
-  .service('googleGrecaptcha', ['$q', '$window', '$document', function GoogleGrecaptchaService($q, $window, $document) {
-    var deferred = $q.defer();
+angular
+  .module('noCAPTCHA', [])
+  .service('googleGrecaptcha', [
+    '$q',
+    '$window',
+    '$document',
+    '$rootScope',
+    function googleGrecaptchaService($q, $window, $document, $rootScope){
+      var deferred = $q.defer();
 
-    $window.recaptchaOnloadCallback = function () {
-      deferred.resolve();
-    };
+      $window.recaptchaOnloadCallback = function (){
+        $rootScope.$apply(function (){
+          deferred.resolve();
+        });
+      };
 
-    var s = $document[0].createElement('script');
-    s.src = 'https://www.google.com/recaptcha/api.js?onload=recaptchaOnloadCallback&render=explicit';
-    $document[0].body.appendChild(s);
+      var s = $document[0].createElement('script');
+      s.src = 'https://www.google.com/recaptcha/api.js?onload=recaptchaOnloadCallback&render=explicit';
+      $document[0].body.appendChild(s);
 
-    return deferred.promise;
-  }])
-  .provider('noCAPTCHA', function NoCaptchaProvider() {
+      return deferred.promise;
+    }])
+  .provider('noCAPTCHA', function noCaptchaProvider(){
     var siteKey,
       theme;
 
-    this.setSiteKey = function(_siteKey){
+    this.setSiteKey = function (_siteKey){
       siteKey = _siteKey;
     };
 
-    this.setTheme = function(_theme){
+    this.setTheme = function (_theme){
       theme = _theme;
     };
 
-    this.$get = [function NoCaptchaFactory() {
+    this.$get = [function noCaptchaFactory(){
       return {
         theme: theme,
         siteKey: siteKey
       }
     }];
   })
-  .directive('noCaptcha', ['noCAPTCHA','googleGrecaptcha', function(noCaptcha, googleGrecaptcha){
-    return {
-      restrict:'EA',
-      scope: {
-        gRecaptchaResponse:'=',
-        siteKey:'@',
-        theme:'@',
-        control:'=',
-        expiredCallback: '='
-      },
-      replace: true,
-      link: function(scope, element) {
-        scope.control = scope.control || {};
-        
-        var widgetId,
-          grecaptchaCreateParameters,
-          control = scope.control;
+  .directive('noCaptcha', [
+    'noCAPTCHA',
+    'googleGrecaptcha',
+    function (noCaptcha, googleGrecaptcha){
+      return {
+        restrict: 'EA',
+        scope: {
+          gRecaptchaResponse: '=',
+          siteKey: '@',
+          theme: '@',
+          control: '=?',
+          expiredCallback: '=?'
+        },
+        replace: true,
+        link: function (scope, element){
+          scope.control = scope.control || {};
 
-        grecaptchaCreateParameters = {
-          sitekey: scope.siteKey || noCaptcha.siteKey,
-          theme: scope.theme || noCaptcha.theme,
-          callback: function(r){
-            scope.$apply(function(){
-              scope.gRecaptchaResponse = r;
-            });
-          },
-          'expired-callback': function () {
-            if (scope.expiredCallback && typeof (scope.expiredCallback) === 'function') {
-              scope.$apply(function () {
-                scope.expiredCallback();
+          var widgetId;
+          var grecaptchaCreateParameters;
+          var control = scope.control;
+
+          grecaptchaCreateParameters = {
+            sitekey: scope.siteKey || noCaptcha.siteKey,
+            theme: scope.theme || noCaptcha.theme,
+            callback: function (r){
+              scope.$apply(function (){
+                scope.gRecaptchaResponse = r;
               });
+            },
+            'expired-callback': function (){
+              if(scope.expiredCallback && typeof (scope.expiredCallback) === 'function') {
+                scope.$apply(function (){
+                  scope.expiredCallback();
+                });
+              }
             }
-          }
-        };
-
-        if(!grecaptchaCreateParameters.sitekey){
-          throw new Error('Site Key is required');
-        }
-
-        googleGrecaptcha.then(function(){
-          widgetId = grecaptcha.render(
-            element[0],
-            grecaptchaCreateParameters
-          );
-          control.reset = function(){
-            grecaptcha.reset(widgetId);
-            scope.gRecaptchaResponse = null;
           };
-        });
 
-        scope.$on('$destroy', function(){
-          grecaptcha.reset(widgetId);
-        });
-      }
-    };
-  }]);
+          if(!grecaptchaCreateParameters.sitekey) {
+            throw new Error('Site Key is required');
+          }
+
+          googleGrecaptcha.then(function (){
+            widgetId = grecaptcha.render(
+              element[0],
+              grecaptchaCreateParameters
+            );
+            control.reset = function (){
+              grecaptcha.reset(widgetId);
+              scope.gRecaptchaResponse = null;
+            };
+          });
+
+          scope.$on('$destroy', function (){
+            grecaptcha.reset(widgetId);
+          });
+        }
+      };
+    }]);
