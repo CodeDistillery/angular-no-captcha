83e116fda968dd28be66c07bcd2ff52fc31754ac
Veikko Karsikko <veikko88@gmail.com>
2015-07-01 12:36:50 +0300

    Test correct script elements are appended to body

--- tests/google-grecaptcha.provider.test.mocha.js
+++ tests/google-grecaptcha.provider.test.mocha.js
@@ -2,16 +2,41 @@ describe('googleGrecaptcha provider', function (){
   beforeEach(module('noCAPTCHA'));
 
   var googleGrecaptcha;
+  var $document;
 
-  beforeEach(inject(function (_googleGrecaptcha_){
-    googleGrecaptcha = _googleGrecaptcha_;
-  }));
+  describe('default options', function (){
+    beforeEach(function(){
+      inject(function (_googleGrecaptcha_, _$document_){
+        $document = _$document_;
+        googleGrecaptcha = _googleGrecaptcha_;
+      });
+    });
 
-  describe('return promise', function (){
+    it('should add script element with correct url', function (){
+      expect($document.find('body').html()).to.contain('<script src="https://www.google.com/recaptcha/api.js?onload=recaptchaOnloadCallback&amp;render=explicit"></script>');
+    });
+
     it('should resolve promise', function (done){
       googleGrecaptcha.then(function (){
         done();
       });
+    });
+  });
+
+  describe('with specific language', function (){
+    beforeEach(function(){
+      module(function(googleGrecaptchaProvider){
+        googleGrecaptchaProvider.setLanguage('fi');
+      });
+
+      inject(function (_googleGrecaptcha_, _$document_){
+        $document = _$document_;
+        googleGrecaptcha = _googleGrecaptcha_;
+      });
+    });
+
+    it('should add script element with correct url', function (){
+      expect($document.find('body').html()).to.contain('<script src="https://www.google.com/recaptcha/api.js?onload=recaptchaOnloadCallback&amp;render=explicit&amp;hl=fi"></script>')
     });
   });
 });
